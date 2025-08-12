44d310aa034987238ecec94510ac08fa9af3a3fb
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-08-11 22:08:39 -0300

    feat: implementação de rotas, controllers e função que exclui produto

--- index.js
+++ index.js
@@ -1,79 +1,27 @@
 // Importações necessárias para o servidor
-const express = require('express')
+const express = require('express');
+
 const mongoose = require('mongoose');
-const Product = require('./models/product.model') // Importa o model do produto
+
+const Product = require('./models/product.model.js'); // Importa o model do produto
+
+const productRoute = require('./routes/product.route.js');
+
 require('dotenv').config(); // Carrega variáveis do .env
 
 const app = express()
 
 app.use(express.json()); // Permite que o Express entenda JSON no corpo das requisições
 
+
+//routes
+app.use('/api/products', productRoute);
+
+
 // Rota raiz
 app.get('/', (req, res) => {
   res.send('Hello from Node API Server Updated');
 });
-
-//All products
-app.get('/api/products', async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products)
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-
-})
-
-//Get By Id
-app.get('/api/products/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product)
-  } catch (error) {
-    res.status(500).json({ messege: error.message })
-  }
-})
-
-
-// Created Product
-app.post('/api/products', async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product)
-  } catch (error) {
-    res.status(500).json({ messege: error.message });
-  }
-});
-
-//update a product
-app.put('/api/product/:id', async(req, res) =>{
-  try {
-    const {id} = req.params;
-
-    const product = await Product.findByIdAndUpdate(id, req.body);
-
-    if(!product){
-      return res.status(404).json({menssage: "Product not found"});
-    }
-
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct)
-
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-})
-
-// delete a product
-
-app.delete('/api/product/:id', async (req, resp) =>{
-  try {
-    
-  } catch (error) {
-    
-  }
-})
 
 
 mongoose.connect(process.env.DB_URI)
